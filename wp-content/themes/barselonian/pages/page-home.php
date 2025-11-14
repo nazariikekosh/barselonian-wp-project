d9b304ab8cba8cf71168b18a91d6917d7ea63959
PORTAL <118050634+nazariikekosh@users.noreply.github.com>
2025-11-14 12:36:45 +0200

    add Home Page, footer, header

---
+++ wp-content/themes/barselonian/pages/page-home.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Template Name: Home Page
+ * 
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package barselonian
+ */
+
+get_header();
+
+
+echo '<h1>Welcome to the Home Page</h1>';
+
+get_footer();
